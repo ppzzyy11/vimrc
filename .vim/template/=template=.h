a70a72a797263f7ad6280d7ced776c7803f9bfba
puzeyu <puzeyu@bupt.edu.cn>
2019-05-06 22:41:54 +0800

    backup

---
+++ .vim/template/=template=.h
@@ -0,0 +1,22 @@
+/*
+ * %FFILE%
+ * Copyright (C) %DATE% %USER% <%MAIL%>
+ *
+ * Distributed under terms of the %LICENSE% license.
+ *
+ * Description:
+ *
+ *      To implement:
+ *
+ *      How:
+ *
+ *      To optimize:
+ *
+ */
+
+#ifndef %GUARD%
+#define %GUARD%
+
+%HERE%
+
+#endif /* !%GUARD% */
